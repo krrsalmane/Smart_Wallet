7c29d92c8653534dd24cf370dc23aa09f8ea804e
krrsalmane <karroumsalmane@gmail.com>
2025-12-23 17:27:29 +0100

    create attributes and getters and setters for class operation

--- entity/operation.php
+++ entity/operation.php
@@ -7,7 +7,48 @@ class operation
     protected string $description;
     protected String  $my_date;
 
+     public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+
+    public function getAmount(): float
+    {
+        return $this->amount;
+    }
+
+    public function setAmount(float $amount): void
+    {
+        $this->amount = $amount;
+    }
+
     
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+
+    public function getMyDate(): string
+    {
+        return $this->my_date;
+    }
+
+    public function setMyDate(string $my_date): void
+    {
+        $this->my_date = $my_date;
+    }
+
 }
 
 ?>
